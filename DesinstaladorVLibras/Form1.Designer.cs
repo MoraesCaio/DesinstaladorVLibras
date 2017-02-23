e73633b9896b713d9198eaa8ddf1ece537d4185a
MoraesCaio <caiomoraes@msn.com>
2017-02-23 10:56:22 -0300

    First Commit

--- DesinstaladorVLibras/Form1.Designer.cs
+++ DesinstaladorVLibras/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Deseja desinstalar o VLibras?";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(18, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sim";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(131, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Não";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(225, 64);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(249, 71);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Desinstalador VLibras";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- DesinstaladorVLibras/Form1.cs
+++ DesinstaladorVLibras/Form1.cs
@@ -1,20 +1,176 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
+using IWshRuntimeLibrary;
 
+/*This program uninstalls VLibras application following six steps:
+ * 1º Deletes the shortcut for VLibras Player;
+ * 2º Deletes the three main folders of the application;
+ * 3º Deletes environment variables created by the application;
+ * 4º Deletes ClickOnce's version files;
+ * 5º Deletes registry entries;
+ * 6º Delete stat menu entries.
+ * Author: Caio Moraes
+ * Email: caiomoraes@msn.com
+ * GitHub: MoraesCaio
+ */
 namespace DesinstaladorVLibras
 {
     public partial class Form1 : Form
     {
+        /*Deletes the specified user environment variable.
+         * Parameter: (string) name of the variable
+         */
+        private static void deleteEnvVarUSR(string variable)
+        {
+            try
+            {
+                Console.WriteLine("Variável " + variable + ":");
+                string t = Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.User);
+                if (t == null)
+                {
+                    Console.WriteLine(variable + " não existe.");
+                }
+                else
+                {
+                    Console.WriteLine(Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.User));
+                    Environment.SetEnvironmentVariable(variable, "", EnvironmentVariableTarget.User);
+                    Console.WriteLine("Apagando.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro {0}", e);
+            }
+        }
+
+        private static void deleteDirectory(string directory){
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.Arguments = "/C rmdir /S /Q " + directory;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.CreateNoWindow = true;
+            Process.Start(info).WaitForExit();
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result1 = MessageBox.Show("Isso pode levar alguns minutos. Por favor, aperte 'Sim' para continuar e aguarde o aviso de desinstalação concluída.", "Aviso!", MessageBoxButtons.YesNo);
+            if(result1 == DialogResult.Yes){
+                string vlibrasPath = null;
+                string rootPath = null;
+                string pythonPath = null;
+                string bundlesPath = null;
+                string aelius_data = null;
+                string hunpos_tagger = null;
+                string nltk_data = null;
+                string translate_data = null;
+                //RETRIEVING PATHS
+                try{
+                    //FOLDERS
+                    vlibrasPath = Environment.GetEnvironmentVariable("PATH_VLIBRAS", EnvironmentVariableTarget.User);
+                    pythonPath = Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.User);
+                    if(vlibrasPath != null){
+                        rootPath = Directory.GetParent(vlibrasPath).FullName;
+                        bundlesPath = Path.Combine(rootPath, @"Bundles\");
+                    }
+                    //ENVIRONMENT VARIABLES
+                    aelius_data =    Environment.GetEnvironmentVariable("AELIUS_DATA", EnvironmentVariableTarget.User);
+                    hunpos_tagger =  Environment.GetEnvironmentVariable("HUNPOS_TAGGER", EnvironmentVariableTarget.User);
+                    nltk_data =      Environment.GetEnvironmentVariable("NLTK_DATA", EnvironmentVariableTarget.User);
+                    translate_data = Environment.GetEnvironmentVariable("TRANSLATE_DATA", EnvironmentVariableTarget.User);
+                }catch(Exception){
+                    return;
+                }
+                //DELETING SHORTCUT
+                try{
+                    object shDesktop = (object)"Desktop";
+                    WshShell shell = new WshShell();
+                    string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\VLibras.lnk";
+                    System.IO.File.Delete(shortcutAddress);
+                }catch(Exception ex){
+                    MessageBox.Show(ex.Message,
+                        "Remoção de atalho",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation,
+                        MessageBoxDefaultButton.Button1);
+                    return;
+                }
+                //DELETING EXTRACTED FOLDERS
+                try{
+                    if(bundlesPath != null && vlibrasPath != null){
+                        deleteDirectory(bundlesPath);
+                        deleteDirectory(vlibrasPath);
+                        string[] zipPaths = Directory.GetFiles(rootPath, "*.zip");
+                        for(int i = 0; i < zipPaths.Length; i++){
+                            System.IO.File.Delete(zipPaths[i]);
+                        }
+                    }
+                    if(pythonPath != null){
+                        deleteDirectory(pythonPath);
+                    }
+                }catch(Exception ex){
+                    MessageBox.Show(ex.Message,
+                        "Remoção de conteúdo extraído",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation,
+                        MessageBoxDefaultButton.Button1);
+                    return;
+                }
+                //DELETING ENVIRONMENT VARIABLES
+                try{
+                    deleteEnvVarUSR("PATH_VLIBRAS");
+                    deleteEnvVarUSR("AELIUS_DATA");
+                    deleteEnvVarUSR("HUNPOS_TAGGER");
+                    deleteEnvVarUSR("NLTK_DATA");
+                    deleteEnvVarUSR("TRANSLATE_DATA");
+                    deleteEnvVarUSR("PYTHONPATH");
+                }catch(Exception ex){
+                    MessageBox.Show(ex.Message,
+                        "Configuração de variáveis",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation,
+                        MessageBoxDefaultButton.Button1);
+                    return;
+                }
+                //DELETING CLICKONCE'S VERSIONS AND REGISTRIES
+                try{
+                    var uninstallInfo = UninstallInfo.Find("Atualizador VLibras");
+                    if (uninstallInfo != null)
+                    {
+                        var uninstaller = new Uninstaller();
+                        uninstaller.Uninstall(uninstallInfo);
+                    }
+                }catch(Exception ex){
+                    MessageBox.Show(ex.Message,
+                        "Desinstalação do atualizador",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation,
+                        MessageBoxDefaultButton.Button1);
+                    return;
+                }
+                //UNINSTALL FINISHED
+                MessageBox.Show(@"Desinstalação concluída.", @"Operação concluída.");
+                //DELETE ITSELF IN 3 SECONDS
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = @"cmd.exe";
+                info.Arguments = @"/C ping 1.1.1.1 -n 1 -w 3000 > Nul & Del DesinstaladorVLibras.exe";
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+                info.CreateNoWindow = true;
+                Process.Start(info);
+                Close();
+            }
         }
     }
 }
